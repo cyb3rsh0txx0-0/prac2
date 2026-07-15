a1bee831614ecf2ab00e0041b25bccef165e51d4
cyb3rsh0txx0-0 <cyb3rsh0txx@gmail.com>
2026-07-15 12:58:17 -0500

    cambiando todo el archivo de main a solo un print

--- helou.py
+++ helou.py
@@ -1,12 +1,1 @@
-for i in range(1,10):
-	print(f"hola numero {i}")
-
-
-cont=0
-while  cont < 10:
-	print(cont)
-	cont += 1
-
-
-
-print("dam hello brhu")
+print("unico aporte que tendra actualmente main")
